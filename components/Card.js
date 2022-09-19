4dea78e11cb4e8945b647557f68abc11a9de03c2
edgardiaz59 <edgardiaz1829@gmail.com>
2022-09-18 21:47:48 -0500

    Module2_CardList-Assignment2-Diaz

---
+++ components/Card.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Card = ({item}) => {
+
+
+
+    return (
+            <View style={styles.container}>
+                <View style={[styles.card]}>
+                    <Image
+                        style={styles.cardImage}
+                        source={{ uri: item.image}}
+                    />
+                    <View style={styles.cardContent} >
+                        <View style={styles.cardHeader} >
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Text style={styles.desc}>{item.desc}</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+    )
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 15
+    },
+    card: {
+      borderWidth: 0,
+      minHeight: 300,
+      shadowColor: '#000000',
+      shadowOffset: {
+        wdith: 2,
+        height: 2
+      },
+      shadowOpacity: 0.5,
+      shadowRadius: 4,
+      marginVertical: 12,
+      marginTop: 0,
+      backgroundColor: '#FFFFFF',
+      marginRight: 0,
+      alignSelf: 'stretch'
+    },
+    cardImage: {
+      flex: 1,
+      height: 150,
+      borderRadius: 3
+    },
+    cardContent: {
+      paddingVertical: 15,
+      paddingHorizontal: 15
+    },
+    title: {
+      fontSize: 28
+    },
+    desc: {
+      fontSize: 15
+    },
+    cardHeader: {
+      paddingVertical: 5
+  
+    }
+});
---
+++ data/cards.js
@@ -0,0 +1,20 @@
+export const cards = [
+    {
+        _id: 1,
+        title: 'MCC Module 2a',
+        desc: 'This is Edgar Diaz\'s 1st Card',
+        image: 'https://source.unsplash.com/random/400x300'
+    },
+    {
+      _id: 2,
+      title: 'MCC Module 2b',
+      desc: 'This is Edgar Diaz\'s 2nd Card',
+      image: 'https://source.unsplash.com/random/400x300'
+    },
+    {
+      _id: 3,
+      title: 'MCC Module 2c',
+      desc: 'This is Edgar Diaz\'s 3rd Card',
+      image: 'https://source.unsplash.com/random/400x300'
+    }
+  ];
